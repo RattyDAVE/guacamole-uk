6e5436b8e04869f601ca8c84c037cefcb257ca8c
RattyDAVE <dave@pucknell.co.uk>
2018-10-31 12:09:14 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Start tomcat7 - although it fails
-echo "Tomcat7 start script says it fails to start even when it's successful"
-service tomcat7 start
+echo "Tomcat8 start script says it fails to start even when it's successful"
+service tomcat8 start
 service guacd start
 
-tail -f /var/log/tomcat7/catalina.out &
+tail -f /var/log/tomcat8/catalina.out &
 
 wait
